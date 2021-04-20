31a3b69e0fa5ca9439acf41adff6c55cf12ff6f4
Cameron Fairchild <cameron@fairchild.dev>
2021-04-19 22:55:14 -0400

    changed structure to allow for dependency inj

--- src/db/db.js
+++ src/db/db.js
@@ -1,19 +1,19 @@
 "use strict";
 require("dotenv").config();
 
-let mongoose = require('mongoose');
+module.exports = function (mongoose) {
+    var module = {};
 
-function connect() {
-    mongoose.connect(
-        process.env.MONGO_URI,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    );
-    return mongoose.connection;
-}
+    module.connect = function() {
+        mongoose.connect(
+            process.env.MONGO_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+        return mongoose.connection;
+    }
 
-module.exports = {
-    connect
+    return module;
 }
