4f16fde8adda42e9458487bd5853c848ee32622d
Cameron Fairchild <cameron@fairchild.dev>
2021-04-19 22:06:43 -0400

    changed to http because I dont have a domain

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 "use strict";
 require('dotenv').config();
 let express = require('express');
-let fs = require('fs');
-let { createServer } = require('https');
-const sio = require('socket.io');
+const http = require('http');
+
+const server = http.createServer(app);
+const sio = require('socket.io')(server);
 
 let api = require('./src/routes/api');
 let db = require('./src/db/db');
 
 let app = express();
-
-const server = createServer({
-    key: fs.readFileSync(process.env.KEY_PATH),
-    cert: fs.readFileSync(process.env.CRT_PATH)
-}, app);
-const io = sio.listen(server);
 
 let port = process.env.PORT || 3000;
 
@@ -32,22 +27,14 @@ db_connection.once('open', () => {
     // On database connection
     io.sockets.on('connection', (socket) => {
         require('./src/routes/events')(socket, io);
-
-        return io;
     });
-
-    app.listen(port);
+    
     // catch 404
     app.use(function(req, res, next) {
-        res.status(404);
-
-        if (req.accepts('json')) {
-          res.json({ error: 'Not found' });
-          return;
-        }
-    
-        res.type('txt').send('Not found');
+        res.status(404).send('Not found');
     });
+    
+    server.listen(port);
 });
 
 module.exports = app;
