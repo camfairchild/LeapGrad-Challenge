2b78146459ac0045b5efbd6f404a4490ee1e5340
Cameron Fairchild <cameron@fairchild.dev>
2021-04-19 22:49:24 -0400

    fixed module exports

--- src/db/db.js
+++ src/db/db.js
@@ -3,15 +3,17 @@ require("dotenv").config();
 
 let mongoose = require('mongoose');
 
-module.exports = () => {
-    function connect() {
-        mongoose.connect(
-            process.env.MONGO_URI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }
-        );
-        return mongoose.connection;
-    }
+function connect() {
+    mongoose.connect(
+        process.env.MONGO_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    );
+    return mongoose.connection;
+}
+
+module.exports = {
+    connect
 }
