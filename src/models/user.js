f3d368ff61753e80a2a234262edaf1a246d7c2d0
Cameron Fairchild <cameron@fairchild.dev>
2021-04-24 11:39:33 -0400

    unique username works

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    username: { type : String , unique : true, required : true, dropDups: true },
+    username: { type : String , unique : true, required : true, dropDups: true, index: true },
     password_hash: { type : String , required : true },
     balance: Number,
     portfolio: [{
