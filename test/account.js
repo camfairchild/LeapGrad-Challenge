bae1ac96835bf6a557ec269406ca2d385a7d28c0
Cameron Fairchild <cameron@fairchild.dev>
2021-04-27 12:05:25 -0400

    fixed test created more tests

---
+++ test/account.js
@@ -0,0 +1,100 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+import chai, { expect } from "chai";
+import chai_as_promised from "chai-as-promised";
+import chaiHttp from "chai-http";
+
+chai.use(chai_as_promised);
+chai.use(chaiHttp);
+
+import { connect, registerUser, loginUser, updateBalance, getBalance } from "../src/db/db.js";
+import User from "../src/models/user.js";
+import { server } from "../server.js";
+
+var db_connection;
+
+before(async () => {
+    db_connection = await connect(process.env.MONGO_URI_test); // test db
+    return db_connection;
+});
+
+chai.should();
+
+/** Test
+ */
+describe("account api endpoints", () => {
+
+    describe("user functions", () => {
+        before((done) => {
+            User.collection.createIndex({ "username": 1 }, { unique: true }).then(() => {
+                User.collection.deleteMany().then(() => {
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        
+        afterEach((done) => {
+            User.collection.deleteMany().then(() => {
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+
+        // Integration test of user update balance
+        it("should get user balance", (done) => {
+            registerUser("username", "password").then(() => {
+                // login user
+                // make put to balance endpoint
+                chai.request(server)
+                .put('/api/account/balance')
+                .send({
+                    amount: 10
+                })
+                .auth("username", "password")
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    res.should.have.status(200);
+                    getBalance("username", (err, balance) => {
+                        expect(err).to.be.null;
+                        balance.should.eql(10);
+                    })
+                    done();
+                });
+                
+            }).catch((err) => {
+                done(err);
+            });
+        });
+
+        // Integration test of user get balance
+        it("should get user balance", (done) => {
+            registerUser("username", "password").then(() => {
+                // set user balance
+                updateBalance("username", 10, (err) => {
+                    // login user
+                    // make get to balance endpoint
+                    chai.request(server)
+                    .get('/api/account/balance')
+                    .auth("username", "password")
+                    .end((err, res) => {
+                        expect(err).to.be.null;
+                        res.should.have.status(200);
+                        res.body.should.have.property("balance").eql(10);
+                        done();
+                    });
+                })
+                
+            }).catch((err) => {
+                done(err);
+            });
+        });
+
+
+    });
+});
--- test/db.js
+++ test/db.js
@@ -6,7 +6,7 @@ import chai_as_promised from "chai-as-promised";
 
 chai.use(chai_as_promised);
 
-import { connect, registerUser } from "../src/db/db.js";
+import { connect, getUserByUsername, registerUser, updateBalance, getBalance } from "../src/db/db.js";
 import User from "../src/models/user.js";
 import { UniqueUserError } from "../src/errors/UserRegistrationErrors.js";
 
@@ -21,7 +21,57 @@ chai.should();
 
 /** Test
  */
-describe("user", () => {
+describe("db: user", () => {
+
+    describe("user functions", () => {
+        before((done) => {
+            User.collection.createIndex({ "username": 1 }, { unique: true }).then(() => {
+                User.collection.deleteMany().then(() => {
+                    done();
+                });
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        
+        afterEach((done) => {
+            User.collection.deleteMany().then(() => {
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+
+        // Test registration
+        it("should update user balance", (done) => {
+            registerUser("username", "password").then(() => {
+                updateBalance("username", 10, (err) => {
+                    expect(err).to.be.null;
+                    getUserByUsername("username", (err, user) => {
+                        expect(err).to.be.null;
+                        user.balance.should.equal(10);
+                        done();
+                    });
+                });
+            }).catch((err) => {
+                done(err);
+            })
+        });
+
+        it("should get user balance", () => {
+            registerUser("username", "password").then(() => {
+                updateBalance("username", 10, (err) => {
+                    expect(err).to.be.null;
+                    getBalance("username", (err, balance) => {
+                        expect(err).to.be.null;
+                        balance.should.equal(10);
+                    })
+                });
+            }).catch((err) => {
+                done(err);
+            })
+        });
+    });
 
     describe("user registration", () => {
         before((done) => {
@@ -44,15 +94,16 @@ describe("user", () => {
 
         // Test registration
         it("should register user in database", (done) => {
-            registerUser("username", "password").then(() => {
+            registerUser("test_user", "password").then(() => {
                 // We should find a user with username in the database
                 User.countDocuments({ "username": "username" }, (err, count) => {
                     expect(err).to.be.null;
                     count.should.be.equal(1); // there should be 1 User with username in db
                     done();
                 })
-            })
-            
+            }).catch((err) => {
+                done(err);
+            })            
         });
 
         // Test registration with empty username
@@ -71,12 +122,14 @@ describe("user", () => {
             registerUser("username", "password").then(() => {
                 // attempt to create a second user with same username
                 registerUser("username", "password") // should error
-                .should.be.rejectedWith(UniqueUserError);
+                    .should.be.rejectedWith(UniqueUserError);
                 User.countDocuments({ "username": "username" }, (err, count) => {
                     expect(err).to.be.null;
                     count.should.be.lessThan(2); // less than 2 should exist, i.e. 1
                     done();
                 });
+            }).catch((err) => {
+                done(err);
             });
         });
     });
