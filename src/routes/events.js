647cd17a2a1b7b73a070a2594a435f5f8bb38ce9
Cameron Fairchild <cameron@fairchild.dev>
2021-04-28 22:52:22 -0400

    listens for "changed" Stock schema event

--- src/routes/events.js
+++ src/routes/events.js
@@ -21,7 +21,10 @@ export default function(io) {
     });
 
     io.sockets.on('connection', (socket) => {
-        Stock.schema.on("saved", (doc) => {
+        console.log("connected!!");
+        // listen for changed event
+        // emit updated doc to socket
+        Stock.schema.on("changed", (doc) => {
             socket.emit("update one", doc);
         })       
 
@@ -29,5 +32,30 @@ export default function(io) {
             var arr = await getAllStocks();
             cb(arr);
         });
+
+        socket.on("new", async () => {
+            var t = new Stock({
+                company: "test",
+                ticker: "TEST",
+                price: 2.00
+            });
+            
+            await t.save();
+
+            var f = new Stock({
+                company: "free",
+                ticker: "FREE",
+                price: 0.00
+            });
+
+            await f.save();
+        });
+
+        socket.on("change", (price) => {
+            Stock.findOne({ ticker: "TEST" }, (err, doc) => {
+                doc.price = price;
+                doc.save();
+            })
+        })
     });   
 }
